ea4ed79758b6fa25b833adbaf1825861e99c9841
“Anoud <anoudalsaidi1993@gmail.com>
2026-05-05 13:58:23 +0400

    Private field is used to protect the rating value

--- Solution01/project01/Program.cs
+++ Solution01/project01/Program.cs
@@ -31,5 +31,23 @@ namespace MiniNetflix
             }
         }
     }
+
+// ===================== WatchSession Class =====================
+class WatchSession
+{
+    public string UserName { get; set; }
+    public string MovieTitle { get; set; }
+
+    public WatchSession(string userName, string movieTitle)
+    {
+        UserName = userName;
+        MovieTitle = movieTitle;
+    }
+}
+
+
+
+
+
     }
 }
