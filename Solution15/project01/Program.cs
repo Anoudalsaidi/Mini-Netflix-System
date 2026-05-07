17700cfc7f97fc45de56e57d58beccf84f02d55d
“Anoud <anoudalsaidi1993@gmail.com>
2026-05-07 10:30:44 +0400

    Added inheritance and overriding example with books classes

---
+++ Solution15/project01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Channels;
+
+namespace project01
+{
+    internal class Program
+    {
+        class books
+        {
+            public void generalbook()
+            {
+                Console.WriteLine("This is general book");
+            }
+
+            public virtual void storybook()
+            {
+                Console.WriteLine("This is story book");
+            }
+
+            public virtual void normalbook()
+            {
+                Console.WriteLine("This is normal book");
+            }
+        }
+
+        class ReadsBook : books
+        {
+            public override void storybook()
+            {
+                Console.WriteLine("Reeding is a story books");
+            }
+
+            public override void normalbook()
+            {
+                Console.WriteLine("Writing is a normal books");
+            }
+        }
+        static void Main(string[] args)
+        {
+           ReadsBook bok = new ReadsBook();
+            
+           
+            bok.storybook();
+            bok.normalbook();
+
+        }
+    }
+}
